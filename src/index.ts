cac90fdb7010751650e96f9ef5460fb5964f1a7c
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2025-01-23 21:43:50 +0000

    +

--- src/index.ts
+++ src/index.ts
@@ -12,75 +12,89 @@
  */
 const validUUID = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i
 
-export default {
-  async fetch(request, env) {
-    if (request.method === "POST") {
-      // @ts-ignore
-      const url = `https://api.airtable.com/v0/${env.AIRTABLE_BASE_ID}/${env.AIRTABLE_TABLE_NAME}`
-      // @ts-ignore
-      const auth = `Bearer ${env.AIRTABLE_API_KEY}`
+const handlePost: ExportedHandlerFetchHandler<Env, unknown> = async (request, env) => {
+  // @ts-ignore
+  const url = `https://api.airtable.com/v0/${env.AIRTABLE_BASE_ID}/${env.AIRTABLE_TABLE_NAME}`
+  // @ts-ignore
+  const auth = `Bearer ${env.AIRTABLE_API_KEY}`
 
-      try {
-        const jsonData = await request.json()
-        if (typeof jsonData !== "object" || jsonData === null) throw new Error("JSON data is not an object")
-        // Validate that all fields are present
-        if (!("user_id" in jsonData)) throw new Error("Missing 'user_id' field")
-        if (!("score" in jsonData)) throw new Error("Missing 'score' field")
-        if (!("username" in jsonData)) throw new Error("Missing 'username' field")
-        // Validate data types and formats
-        if (typeof jsonData.user_id !== "string") throw new Error("User ID is not a string")
-        if (!validUUID.test(jsonData.user_id)) throw new Error("User ID is not a valid UUID")
-        if (typeof jsonData.score !== "number") throw new Error("Score is not a number")
-        if (jsonData.score < 0) throw new Error("Score is negative")
-        if (typeof jsonData.username !== "string") throw new Error("Username is not a string")
-        if (jsonData.username.length === 0) throw new Error("Username is empty")
-        // Ensure consistency in the database by normalising the values
-        const username = jsonData.username.trim().slice(0, 255)
-        const score = jsonData.score
-        const userId = jsonData.user_id.toLowerCase()
+  try {
+    const jsonData = await request.json()
+    if (typeof jsonData !== "object" || jsonData === null) throw new Error("JSON data is not an object")
+    // Validate that all fields are present
+    if (!("user_id" in jsonData)) throw new Error("Missing 'user_id' field")
+    if (!("score" in jsonData)) throw new Error("Missing 'score' field")
+    if (!("username" in jsonData)) throw new Error("Missing 'username' field")
+    // Validate data types and formats
+    if (typeof jsonData.user_id !== "string") throw new Error("User ID is not a string")
+    if (!validUUID.test(jsonData.user_id)) throw new Error("User ID is not a valid UUID")
+    if (typeof jsonData.score !== "number") throw new Error("Score is not a number")
+    if (jsonData.score < 0) throw new Error("Score is negative")
+    if (typeof jsonData.username !== "string") throw new Error("Username is not a string")
+    if (jsonData.username.length === 0) throw new Error("Username is empty")
+    // Ensure consistency in the database by normalising the values
+    const username = jsonData.username.trim().slice(0, 255)
+    const score = jsonData.score
+    const userId = jsonData.user_id.toLowerCase()
 
-        // Append the data to our Airtable base
-        console.log(`Sending PATCH to ${url}`)
-        const airtableResponse = await fetch(url, {
-          method: "PATCH",
-          headers: {
-            Authorization: auth,
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            performUpsert: {
-              fieldsToMergeOn: ["User ID"],
+    // Append the data to our Airtable base
+    console.log(`Sending PATCH to ${url}`)
+    const airtableResponse = await fetch(url, {
+      method: "PATCH",
+      headers: {
+        Authorization: auth,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        performUpsert: {
+          fieldsToMergeOn: ["User ID"],
+        },
+        records: [
+          {
+            fields: {
+              Username: username,
+              Score: score,
+              "User ID": userId,
             },
-            records: [
-              {
-                fields: {
-                  Username: username,
-                  Score: score,
-                  "User ID": userId,
-                },
-              },
-            ],
-          }),
-        })
+          },
+        ],
+      }),
+    })
 
-        if (!airtableResponse.ok) {
-          console.error(await airtableResponse.json())
-          throw new Error("Failed to save to Airtable")
-        }
-
-        const airtableData = await airtableResponse.json()
-        return new Response(JSON.stringify(airtableData), {
-          status: 200,
-          headers: { "Content-Type": "application/json" },
-        })
-      } catch (error: any) {
-        return new Response(JSON.stringify({ error: error.message }), {
-          status: 500,
-          headers: { "Content-Type": "application/json" },
-        })
-      }
-    } else {
-      return new Response("Woah there, method not allowed!", { status: 405 })
+    if (!airtableResponse.ok) {
+      console.error(await airtableResponse.json())
+      throw new Error("Failed to save to Airtable")
     }
+
+    const airtableData = await airtableResponse.json()
+    return new Response(JSON.stringify(airtableData), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    })
+  } catch (error: any) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    })
+  }
+}
+
+const handleGet: ExportedHandlerFetchHandler<Env, unknown> = async (request, env) => {
+  // @ts-ignore
+  const url = `https://api.airtable.com/v0/${env.AIRTABLE_BASE_ID}/${env.AIRTABLE_TABLE_NAME}`
+  // @ts-ignore
+  const auth = `Bearer ${env.AIRTABLE_API_KEY}`
+}
+
+export default {
+  async fetch(request, env, ctx) {
+    if (request.method === "POST") {
+      return handlePost(request, env, ctx)
+    }
+    if (request.method === "GET") {
+      return handleGet(request, env, ctx)
+    }
+
+    return new Response("Woah there, method not allowed!", { status: 405 })
   },
 } satisfies ExportedHandler<Env>
