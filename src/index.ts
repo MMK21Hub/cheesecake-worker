e86c7a701f14e1b7411e06c8b998c64eb9fa32cd
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2025-01-24 08:38:56 +0000

    Add a getHandler() function

--- src/index.ts
+++ src/index.ts
@@ -10,9 +10,11 @@
  *
  * Learn more at https://developers.cloudflare.com/workers/
  */
+type RequestHandler = (request: Request<unknown, IncomingRequestCfProperties<unknown>>, env: Env) => Promise<Response>
+
 const validUUID = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i
 
-const handlePost: ExportedHandlerFetchHandler<Env, unknown> = async (request, env) => {
+const handlePost: RequestHandler = async (request, env) => {
   // @ts-ignore
   const url = new URL(`https://api.airtable.com/v0/${env.AIRTABLE_BASE_ID}/${env.AIRTABLE_TABLE_NAME}`)
   // @ts-ignore
@@ -34,7 +36,7 @@ const handlePost: ExportedHandlerFetchHandler<Env, unknown> = async (request, en
     if (jsonData.username.length === 0) throw new Error("Username is empty")
     // Ensure consistency in the database by normalising the values
     const username = jsonData.username.trim().slice(0, 255)
-    const score = jsonData.score
+    const score = Math.floor(jsonData.score) // Ensure score is an integer
     const userId = jsonData.user_id.toLowerCase()
 
     // Append the data to our Airtable base
@@ -79,7 +81,7 @@ const handlePost: ExportedHandlerFetchHandler<Env, unknown> = async (request, en
   }
 }
 
-const handleGet = async (request: Request<unknown, IncomingRequestCfProperties<unknown>>, env: Env): Promise<Response> => {
+const handleGet: RequestHandler = async (_, env): Promise<Response> => {
   // @ts-ignore
   const url = new URL(`https://api.airtable.com/v0/${env.AIRTABLE_BASE_ID}/${env.AIRTABLE_TABLE_NAME}`)
   // @ts-ignore
@@ -105,21 +107,21 @@ const handleGet = async (request: Request<unknown, IncomingRequestCfProperties<u
   })
 }
 
+function getHandler(request: Request<unknown, IncomingRequestCfProperties<unknown>>): RequestHandler | null {
+  if (request.method === "POST") return handlePost
+  if (request.method === "GET") return handleGet
+  return null
+}
+
 export default {
   async fetch(request, env, ctx) {
-    if (request.method === "POST") {
-      return handlePost(request, env, ctx)
-    }
-    if (request.method === "GET") {
-      return handleGet(request, env).catch(
-        (error) =>
-          new Response(JSON.stringify({ error: error.message }), {
-            status: 500,
-            headers: { "Content-Type": "application/json" },
-          })
-      )
-    }
-
-    return new Response("Woah there, method not allowed!", { status: 405 })
+    const handler = getHandler(request)
+    if (!handler) return new Response("Woah there, method not allowed!", { status: 405 })
+    return handler(request, env).catch((error) => {
+      return new Response(JSON.stringify({ error: error.message }), {
+        status: 500,
+        headers: { "Content-Type": "application/json" },
+      })
+    })
   },
 } satisfies ExportedHandler<Env>
